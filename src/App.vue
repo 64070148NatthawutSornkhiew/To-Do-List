bcc2ce577dfa8d11c625c312fbbe67c66a09195f
FixFire <89625074+64070148NatthawutSornkhiew@users.noreply.github.com>
2023-01-07 16:17:55 +0700

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <div class="td-form"> -->
+  <FormComponent @save="keep" :tdList="tdList"></FormComponent>
+  <div>
+    <p>You have {{ showleft }} things to do</p>
+  </div>
+  <!-- </div> -->
+
+  <ul>
+    <li
+      v-for="(list, index) in reverseList"
+      :key="list.id"
+      @click="doneIt(list)"
+      :class="{ line: list.checked }"
+    >
+      <!-- :class="{line : list.checked}" จะทำ class นั้นๆ เมื่อ list.checked เป็น true -->
+      <span class="index">{{ index + 1 }} .</span> {{ list.text }}
+      <div v-if="list.checked" class="btn">
+        <button @click="remove(list)">Delete</button>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import FormComponent from "./components/FormComponent.vue";
 export default {
-  name: 'App',
+  name: "app",
+  data() {
+    return {
+      tdList: [],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    FormComponent,
+  },
+  methods: {
+    keep(list) {
+      this.tdList.push(list);
+    },
+    doneIt(list) {
+      list.checked = !list.checked;
+    },
+    remove(list) {
+      this.tdList = this.tdList.filter((item) => {
+        return item.id !== list.id;
+      });
+    },
+  },
+  computed: {
+    reverseList() {
+      return [...this.tdList].reverse();
+    },
+    showleft() {
+      return this.tdList.length;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap');
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Slabo 27px', serif;
+  background-color: aquamarine;
+}
+.line {
+  text-decoration: line-through;
+}
+ul li {
+  font-size: 1.5rem;
+  cursor: pointer;
+  margin-bottom: 0.5rem;
+  width: fit-content;
+  margin-left: 37%;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin-top: 2rem;
+}
+.index {
+  margin-right: 0.5rem;
+}
+.btn{
+  text-align: end ;
 }
 </style>
---
+++ src/components/FormComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="saveData">
+    <div>
+      <h1>To Do List App</h1>
+      <input type="text" v-model="text" />
+      <button :disabled="text.length < 1">Add to list</button>
+    </div>
+  </form>
+</template>
+<script>
+let cnt = 0;
+export default {
+  name: "FormInput",
+  data() {
+    return {
+      text: "",
+    };
+  },
+  props: ["tdList"],
+  methods: {
+    saveData() {
+      const newData = {
+        id: cnt++,
+        text: this.text,
+        checked: false,
+      };
+      this.$emit("save", newData);
+      this.resetData();
+    },
+    resetData() {
+      this.text = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+input{
+    width: 20%;
+  }
+  button{
+    margin: 0 0 0 0.5rem;
+    
+  }
+</style>
